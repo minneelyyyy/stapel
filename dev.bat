8174f7787ce72ec5d2a196bea9ce83a585f10d8c
minneelyyyy <abigail@minneelyyyy.dev>
2025-11-28 20:31:46 -0500

    remove now redundant build scripts

--- dev.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rmdir /s /q build
-cmake -S . -B build -G Ninja -DCMAKE_INSTALL_PREFIX=build\out\StapelSDK
--- dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -rf build/
-cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=build/out/StapelSDK
